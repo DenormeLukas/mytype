18c5c86521ca9687ba4ff066770876d4847b7252
DenormeLukas <lukas.denorme@student.howest.be>
2022-01-22 17:41:37 +0100

    Working on html and some experiments '#1'

---
+++ src/js/functions/hover.js
@@ -0,0 +1,22 @@
+let container = document.querySelector('.fullscreen');
+
+
+setInterval(function () {
+
+    this.classList.remove('fullscreen');
+    this.classList.add('fullscreen2');
+
+}, 2000);
+
+
+setInterval(function () {
+
+    this.classList.remove('fullscreen');
+    this.classList.add('fullscreen2');
+
+}, 4000);
+
+
+
+
+
