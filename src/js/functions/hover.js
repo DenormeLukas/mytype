493dd93e0fbd552ec93851f3a21455428c309588
DenormeLukas <lukas.denorme@student.howest.be>
2022-01-26 13:40:16 +0100

    reverse hover every 4 seconds '#3'

--- src/js/functions/hover.js
+++ src/js/functions/hover.js
@@ -1,17 +1,28 @@
 {
+
+    let i = 0;
+
     function hover() {
 
         $(".hover").hover(
             function () {
                 $(this).addClass("hovered");
+
             }
         )
 
     }
 
+    function deleteClass() {
+        let timer = setInterval(function () {
+            $(".hovered").removeClass("hovered");
+        }, 4000);
+    }
+
     const init = () => {
 
         hover();
+        deleteClass();
 
     }
 
