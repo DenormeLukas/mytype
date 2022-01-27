575889899e4a3bcc4dcf94fcdae21e950559e60b
DenormeLukas <lukas.denorme@student.howest.be>
2022-01-27 11:49:42 +0100

    added database '#7'

--- src/bootstrap/database.php
+++ src/bootstrap/database.php
@@ -1,15 +1,17 @@
 <?php
+
 use Illuminate\Database\Capsule\Manager as Capsule;
+
 $capsule = new Capsule;
 $capsule->addConnection([
- 'driver'    => 'mysql',
- 'host'      => getenv('PHP_DB_HOST') ?: 'mysql',
- 'database'  => getenv('PHP_DB_DATABASE') ?: 'webpack-php-template',
- 'username'  => getenv('PHP_DB_USERNAME') ?: 'root',
- 'password'  => getenv('PHP_DB_PASSWORD') ?: 'devine4life',
- 'charset'   => 'utf8mb4',
- 'collation' => 'utf8mb4_unicode_ci',
- 'prefix'    => '',
+    'driver'    => 'mysql',
+    'host'      => getenv('PHP_DB_HOST') ?: 'ID310084_mytype.db.webhosting.be',
+    'database'  => getenv('PHP_DB_DATABASE') ?: 'ID310084_mytype',
+    'username'  => getenv('PHP_DB_USERNAME') ?: 'ID310084_mytype',
+    'password'  => getenv('PHP_DB_PASSWORD') ?: 'mytypeint3',
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
